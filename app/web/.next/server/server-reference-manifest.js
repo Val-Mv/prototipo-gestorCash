6847b54ca91ba6ff5e492130c9b812e529819407
Karina Sierra Velandia <kkarinasierrav@gmail.com>
2025-11-05 21:13:39 -0500

    ajustes 3

--- app/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"a4VghAqu29RD+Jyhfpfpas/+nw5jB6SOFVpicSI4GxA=\"\n}"
--- app/web/.next/static/chunks/src_app_dashboard_page_tsx_8d8a391d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_dashboard_sales-chart-content_tsx_2e60c193._.js",
-  "static/chunks/_e535e311._.js"
-],
-    source: "dynamic"
-});
